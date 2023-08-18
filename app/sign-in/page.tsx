393a0e66549b39bb8c13e691b88a0084945c09dc
Krishna Gupta <129900110+iamkrishnagupta10@users.noreply.github.com>
2023-08-18 12:42:34 -0700

    Delete app/sign-in directory

--- app/sign-in/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default async function SignInPage() {
-  // Redirecting to home as login is no longer required
-  redirect('/')
-  return null
-}
